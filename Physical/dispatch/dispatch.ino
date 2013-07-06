9d1322fce192e12b4e9454ec888e0346cd968e5f
Aidan Seine <aidanseine@gmail.com>
2013-07-06 14:18:47 -0700

    housecleaning: removed clutter, added DEBUG macro

--- Physical/dispatch/dispatch.ino
+++ Physical/dispatch/dispatch.ino
@@ -1,5 +1,5 @@
 #include <EasyTransfer.h>
-
+#define DEBUG
 
 enum states{
  RESTING,
@@ -14,9 +14,6 @@ struct SEND_DATA_STRUCTURE{
   //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
   
   uint8_t state;
-//  int nitrogen;
-//  int ph;
-//  int temperature;
   uint8_t nitrogen;
   uint8_t ph;
   uint8_t temperature;
@@ -28,13 +25,6 @@ EasyTransfer ET;
 boolean winning = false;
 
 SEND_DATA_STRUCTURE mydata;
-//
-//
-//uint8_t state = 0;
-//uint8_t nitrogen = 0;
-//uint8_t ph = 0;
-//
-//uint8_t temperature = 0;
 
 void setup()
 {
@@ -56,39 +46,14 @@ void loop()
   mydata.state = 1;
   //human_input(); //only while testing, in deployment will use rotary encoders.
   getEncoderData();  
-  //Serial.println(mydata.state);
+#ifdef DEBUG
+   Serial.println(mydata.state);
+#endif
   ET.sendData();
   //writeState();
   int balance = abs(mydata.temperature - 127) + abs(mydata.ph - 127)  + abs(mydata.nitrogen -127);
   //if (mydata.state != GAME) Serial.write(0);
   
-  
-  //balance max = 381
-  //balance mid = 190
-  //if balance is less than 25 you win
-//  
-//  if (balance > 30)
-//  { 
-//    Serial.write(1);
-//  }
-//  else
-//  {
-//    Serial.write(2);
-//  }
-//  if (balance > 30) {
-//  } else if (balance < 30) {
-//  }
-  //else Serial.write(3);
-  //else Serial.write(0);
-  //Serial.write(1);
-//  
-//  if (balance <= 29){
-//    Serial.write(1); 
-//  }
-//  else if ( balance > 29 && balance > 31) Serial.write(2);
-//  else Serial.write(3);
-
-  //for(int i = 0; i < 
   if (balance < 30)
   {
       Serial.write(1);
@@ -118,10 +83,12 @@ void loop()
   
   
   
-  
-//  for(int i = 0; i < 255; ++i)
-//  //Serial.write(mydata.state);
-//  Serial.write(i);
+#ifdef DEBUG  
+  for(int i = 0; i < 255; ++i){
+    Serial.write(mydata.state);
+    Serial.write(i);
+    }
+#endif
   
   delay(10);
 }
@@ -141,18 +108,26 @@ void getEncoderData()
   int sensorValue2 = pulseIn(51, HIGH);
   int sensorValue3 = pulseIn(53, HIGH);
 
-  //Serial.println(sensorValue3);
-//  Serial.println(temperature);
-  //Serial.println(sensorValue);
+#ifdef DEBUG
+  Serial.println(sensorValue3);
+  Serial.println(temperature);
+  Serial.println(sensorValue);
+#endif
+
   diff1 = diff1 + (sensorValue1 - prevValue1) ;
+
   if (abs(diff1) > 600) {
       diff1 = 0;
   }
+
   diff2 = diff2 + (sensorValue2 - prevValue2) ;
+
   if (abs(diff2) > 600) {
       diff2 = 0;
   }
+
   diff3 = diff3 + (sensorValue3 - prevValue3) ;
+
   if (abs(diff3) > 600) {
       diff3 = 0;
   }
@@ -161,88 +136,47 @@ void getEncoderData()
   float weighted2 = diff2 / 8;
   float weighted3 = diff3 / 8;
   
-//  if(weighted1 < 0 && mydata.temperature + weighted1 > mydata.temperature) weighted1 = 0;
-//  if(weighted1 > 0 && mydata.temperature + weighted1 < mydata.temperature) weighted1 = 255;
-//  if(weighted2 < 0 && mydata.temperature + weighted2 > mydata.temperature) weighted2 = 0;
-//  if(weighted2 > 0 && mydata.temperature + weighted2 < mydata.temperature) weighted2 = 255;
-//  if(weighted3 < 0 && mydata.temperature + weighted3 > mydata.temperature) weighted3 = 0;
-//  if(weighted3 > 0 && mydata.temperature + weighted3 < mydata.temperature) weighted3 = 255;
-
-
-
-//  if( mydata.temperature + weighted2 > mydata.temperature) weighted2 = 0;
-//  if(weighted2 > 0 && mydata.temperature + weighted2 < mydata.temperature) weighted2 = 0;
-//  if(weighted3 < 0 && mydata.temperature + weighted3 > mydata.temperature) weighted3 = 0;
-//  if(weighted3 > 0 && mydata.temperature + weighted3 < mydata.temperature) weighted3 = 0;
-//  uint8_t potentialdata1 = mydata.temperature + weighted1;
-//  uint8_t potentialdata2 = mydata.nitrogen + weighted2;
-//  uint8_t potentialdata3 = mydata.ph + weighted3;
-//  
-//  if (abs(potentialdata1 - mydata.temperature < 200)) mydata.temperature = potentialdata1;
-//  if (abs(potentialdata2 - mydata.temperature < 200)) mydata.nitrogen = potentialdata2;
-//  if (abs(potentialdata3 - mydata.temperature < 200)) mydata.ph = potentialdata3;
-
-
-//  if (mydata.temperature > 10 && mydata.temperature < 245) mydata.temperature += weighted1;
-//  if (mydata.nitrogen > 10 && mydata.nitrogen < 245) mydata.nitrogen += weighted2;
-//  if (mydata.ph > 10 && mydata.ph < 245) mydata.ph += weighted3;
-  
-//  
-  //mydata.temperature += weighted1;
   if ((mydata.temperature + weighted1) < 0) {
       mydata.temperature = 0;
-      //Serial.println("temperature reset ");
+      #ifdef DEBUG
+      Serial.println("temperature reset ");
+      #endif
   }
   else if ((mydata.temperature + weighted1) > 255) {
       mydata.temperature = 255;
-      //Serial.println("temperature reset ");
+      #ifdef DEBUG
+      Serial.println("temperature reset ");
+      #endif
   }
   else mydata.temperature += weighted1;
   
   if ((mydata.nitrogen + weighted2) < 0) {
       mydata.nitrogen = 0;
-      //Serial.println("nitrogen reset");
+      #ifdef DEBUG
+      Serial.println("nitrogen reset");
+      #endif
   }
   else if ((mydata.nitrogen + weighted2) > 255) {
       mydata.nitrogen = 255;
-      //Serial.println("temperature reset ");
+      #ifdef DEBUG
+      Serial.println("temperature reset ");
+      #endif
   }
   else mydata.nitrogen += weighted2;
-  //mydata.nitrogen += weighted2;
+
   if ((mydata.ph + weighted3) < 0) {
       mydata.ph = 0;
-      //Serial.println("ph reset");
+      #ifdef DEBUG
+      Serial.println("ph reset");
+      #endif
   }
   else if ((mydata.ph + weighted3) > 255) {
       mydata.ph = 255;
-      //Serial.println("temperature reset ");
+      #ifdef DEBUG
+      Serial.println("temperature reset ");
+      #endif
   }
-
   else mydata.ph += weighted3;
-  
-  //mydata.ph += weighted3;
-  
-
-//  if(mydata.temperature + weighted1 > 255) weighted1 = 255;
-//  if(mydata.temperature + weighted1 < 0) weighted1 = 0;
-//  if(mydata.nitrogen + weighted2 > 255) weighted2 = 255;
-//  if(mydata.nitrogen + weighted2 < 0) weighted2 = 0;
-//  if(mydata.ph + weighted3 > 255) weighted3 = 255;
-//  if(mydata.ph + weighted3 < 0) weighted3 = 0;
-//  if(mydata.temperature > 255) weighted1 = 255;
-//  if(mydata.temperature < 0) weighted1 = 0;
-//  if(mydata.nitrogen > 255) weighted2 = 255;
-//  if(mydata.nitrogen < 0) weighted2 = 0;
-//  if(mydata.ph > 255) weighted3 = 255;
-//  if(mydata.ph < 0) weighted3 = 0;
-
-//  if(mydata.temperature > 255) mydata.temperature = 255;
-//  if(mydata.temperature < 0) mydata.temperature = 0;
-//  if(mydata.nitrogen > 255) mydata.nitrogen = 255;
-//  if(mydata.nitrogen < 0) mydata.nitrogen = 0;
-//  if(mydata.ph > 255) mydata.ph = 255;
-//  if(mydata.ph < 0) mydata.ph = 0;
-
 
   diff1 = diff1 % 8; 
   diff2 = diff2 % 8; 
@@ -251,51 +185,22 @@ void getEncoderData()
   prevValue1 = sensorValue1;
   prevValue2 = sensorValue2;
   prevValue3 = sensorValue3;
-  
-//    Serial.print("nitrogen = ");
-//    Serial.println(mydata.nitrogen);
-//    Serial.print("ph = ");
-//    Serial.println(mydata.ph);
-//    Serial.print("temperature = ");
-//    Serial.println(mydata.temperature);
 
+#ifdef DEBUG
+    Serial.print("nitrogen = ");
+    Serial.println(mydata.nitrogen);
+    Serial.print("ph = ");
+    Serial.println(mydata.ph);
+    Serial.print("temperature = ");
+    Serial.println(mydata.temperature);
+#endif
 
 }
 
 //sends 6 byte message
 void writeState()
 {
-//  int TIME = (int) (millis() % 32000);
-//  uint8_t low = lowByte(TIME);
-//  uint8_t high = highByte(TIME);
-//  Serial1.write(255);
-//  if (low == 255) low = 254; 
-//  if (high == 255) high = 254; 
-//  if (state == 255) state = 254; 
-//  if (nitrogen == 255) nitrogen = 254; 
-//  if (ph == 255) ph = 254; 
-//  if (temperature == 255) temperature = 254; 
-//  if (state == 255) state = 254; 
-////  Serial1.write(low);
-////  Serial1.write(high);
-//  Serial1.write(state);
-//  Serial1.write(nitrogen);
-//  Serial1.write(ph);
-//  Serial1.write(temperature);
-//  Serial.println(state);
-//  /*
-//  Serial.print("low = ");
-//  Serial.println(low, DEC);
-//  //Serial.println(
-//  Serial.print("high = ");
-//  Serial.println(high, DEC);
-//  Serial.print("TIME = ");
-//  Serial.println(TIME, DEC);
-//  Serial.print("TIME SHOULD BE ");
-//  Serial.println(high * 256 + low, DEC);
-//  Serial.println();
-//  */
-   // Serial.write(mydata.state);
+
 }
 
 void human_input()
