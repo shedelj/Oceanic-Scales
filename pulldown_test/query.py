f4e32f047917274dd89fc807f335be675e7bbd38
endeav0ur <tylsmith@gmail.com>
2014-03-06 17:42:41 -0800

    fixed arduino things

--- pulldown_test/query.py
+++ pulldown_test/query.py
@@ -53,6 +53,7 @@ a = [[],[], []]
 index = 0
 for row in reader:
     #print '\t'.join(row)
+    print row
     if len(row) != 5:
         continue
     if index % 24 is not 0:
@@ -61,9 +62,8 @@ for row in reader:
     for i in range(2,5):
         val = float(strip_tags(row[i]))
         diff = buf[i-2][1] - buf[i-2][0]
-        if val > 500000:
-            val = buf[i-2][2]
-        if diff != 0 and (val > buf[i-2][1] + diff / 2 or val < buf[i-2][0] - diff / 2):
+        if val > buf[i-2][1] + diff or val < buf[i-2][0] - diff: 
+            #print 'yup'
             val = buf[i-2][2]
         #print val
         a[i-2].append(val)
@@ -81,15 +81,12 @@ print 'void init_vals(){'
 
 for i in range(0,3):
     index = 0
-    if i == 0: label = '   nitrate_lapse'
-    if i == 1: label = '   temperature_lapse'
-    if i == 2: label = '   ph_lapse'
-    counter = 0
+    if i == 0: label = '   nitrate_lapse['
+    if i == 1: label = '   temperature_lapse['
+    if i == 2: label = '   ph_lapse['
     for val in a[i]:
-        print label + '[' + str(index) + '] = ' + str(int(((val - buf[i][0]) / (buf[i][1] - buf[i][0])) * 255)) + ';'
+        print label + str(index) + '] = ' + str(int(((val - buf[i][0]) / (buf[i][1] - buf[i][0])) * 255)) + ';'
         index += 1
-        counter += 1
-    print label + '_size = ' + str(counter) + ';'
 
 print '}'
 
